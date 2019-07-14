c494b24e354af3f823935f50bbad3b7fb0e9612e
Kenny Maness <Kenny@Shep.attlocal.net>
2019-07-14 17:00:16 -0500

    ModuleNotFoundError

--- main.py
+++ main.py
@@ -1,42 +1,8 @@
 from flask import Flask, request
+from index import *
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-
-form = '''<!DOCTYPE html>
-
-<html>
-    <head>
-        <style>
-            form {{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }}
-            textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }}
-        </style>
-    </head>
-    <body>
-        <form method="post">
-            Encrypt a message!
-            <br>
-            <input type="text" name="rot" placeholder="Enter a number">
-            <br><br>
-            <textarea rows="4" cols="50" name="text" placeholder="Enter your message">{0}</textarea>
-            <br>
-            <button type="submit">submit</button>
-        </form>
-    </body>
-</html>'''
-
-
 
 @app.route("/")
 def index():
