3a7ebeba309e78b5fc84841d180fdb8c89201f30
Kenny Maness <Kenny@Shep.attlocal.net>
2019-07-21 14:27:36 -0500

    format submit() function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template
+import os
+import cgi
 from functions import *
 
 errors = {
@@ -16,22 +18,22 @@ def index():
     return render_template("index.html")
 
 @app.route("/", methods=["POST"])
-def tryagain():
+def submit():
     username = request.form["username"]
     password = request.form["password"]
     passconfirm = request.form["pass_confirm"]
     email = request.form["email"]
 
     if valid_username(username) == False:
-        username_error = "Please enter a valid username, between 3 and 20 characters, with no spaces."
-    elif valid_password(password) == False:
-        password_error = "Don't forget your password!"
-    elif passwords_match(password, passconfirm) == False:
-        passconfirm_error = "Make sure your passwords match."
-    elif valid_email(email) == False:
-        email_error = "Remember to enter your email correctly. Or not at all!"
-
-    return
+        errors["username"] = "Please enter a valid username, between 3 and 20 characters, with no spaces."
+    if valid_password(password) == False:
+        errors["password"] = "Don't forget your password!"
+    if passwords_match(password, passconfirm) == False:
+        errors["pass_confirm"] = "Make sure your passwords match."
+    if valid_email(email) == False:
+        errors["email"] = "Remember to enter your email correctly. Or not at all!"
+    
+    return render_template("index.html", username_error = errors["username"], password_error = errors["password"], passconfirm_error = errors["pass_confirm"], email_error = errors["email"])
 
 @app.route("/welcome", methods=["POST"])
 def welcome():
